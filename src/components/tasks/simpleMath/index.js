be3a44a318e84bbb22d524809ae47d173b86ff9c
KatePavlovich <ekate.pavlovich@gmail.com>
2019-10-04 14:58:02 +0300

    Make counting easier

--- src/components/tasks/simpleMath/index.js
+++ src/components/tasks/simpleMath/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { wasTaskAnsweredAC, checkCorrectAnswerAC } from "../../../ac/taskAC";
-import { Modal } from "antd";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { wasTaskAnsweredAC, checkCorrectAnswerAC } from '../../../ac/taskAC';
+import { Modal } from 'antd';
 import {
   getRandomNumberWithoutZero,
   getRandomValueFromArray
-} from "../../../helperFunctions";
-import "./index.css";
+} from '../../../helperFunctions';
+import './index.css';
 
-const signs = ["+", "-", "*", "/"];
+const signs = ['+', '-', '*', '/'];
 
 class SimpleMath extends Component {
   state = {
-    number1: getRandomNumberWithoutZero(100),
+    number1: getRandomNumberWithoutZero(10),
     number2: getRandomNumberWithoutZero(10),
     sign: getRandomValueFromArray(signs)
   };
@@ -20,7 +20,7 @@ class SimpleMath extends Component {
   compareAnswers = e => {
     let playerAnswer = e.currentTarget.value;
     const { sign, number1, number2 } = this.state;
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       let correctAnswer = parseInt(eval(`${number1}${sign}${number2}`));
       let transformedPlayerAnswer = parseInt(playerAnswer, 10);
       if (transformedPlayerAnswer === correctAnswer) {
@@ -29,7 +29,7 @@ class SimpleMath extends Component {
         this.props.dispatch(checkCorrectAnswerAC(false));
       }
       this.props.dispatch(wasTaskAnsweredAC());
-      playerAnswer = "";
+      playerAnswer = '';
     }
   };
 
@@ -45,15 +45,9 @@ class SimpleMath extends Component {
       >
         <div>
           <div>
-            <span>
-              {number1}
-            </span>
-            <span>
-              {sign}
-            </span>
-            <span>
-              {number2}
-            </span>
+            <span>{number1}</span>
+            <span>{sign}</span>
+            <span>{number2}</span>
             <span>=?</span>
           </div>
           <input
