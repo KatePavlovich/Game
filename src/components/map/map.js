42fb1f409de9701fadcfdfa6bbf9d12fb136fd2a
KatePavlovich <ekate.pavlovich@gmail.com>
2019-10-04 14:56:06 +0300

    Fix unique values by adding key

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { connect } from "react-redux";
-import { SPRITE_BACKGROUND_SIZE } from "../../constants";
-import "./style.css";
+import React from 'react';
+import { connect } from 'react-redux';
+import { SPRITE_BACKGROUND_SIZE } from '../../constants';
+import './style.css';
 
 function getTileSprite(type) {
   switch (type) {
     case 0:
-      return "grass";
+      return 'grass';
     case 1:
-      return "wall";
+      return 'wall';
     case 2:
-      return "prison";
+      return 'prison';
     case 3:
-      return "road-top";
+      return 'road-top';
     case 4:
-      return "road-bottom";
+      return 'road-bottom';
     case 5:
-      return "road-bottom-right";
+      return 'road-bottom-right';
     case 6:
-      return "road-top-left";
+      return 'road-top-left';
     case 7:
-      return "road-left";
+      return 'road-left';
     case 8:
-      return "road-right";
+      return 'road-right';
     case 9:
-      return "road-right-corner";
+      return 'road-right-corner';
     case 10:
-      return "road-left-corner";
+      return 'road-left-corner';
     case 11:
-      return "tree";
+      return 'tree';
     case 12:
-      return "woods";
+      return 'woods';
     default:
   }
 }
@@ -50,7 +50,9 @@ function MapTile(props) {
 function MapRow(props) {
   return (
     <div className="row">
-      {props.tiles.map(tile => <MapTile tile={tile} />)}
+      {props.tiles.map((tile, index) => (
+        <MapTile tile={tile} key={`${tile}_${index}`} />
+      ))}
     </div>
   );
 }
@@ -58,7 +60,9 @@ function MapRow(props) {
 function Map(props) {
   return (
     <div className="map">
-      {props.tiles.map(row => <MapRow tiles={row} />)}
+      {props.tiles.map(row => (
+        <MapRow tiles={row} key={row} />
+      ))}
     </div>
   );
 }
